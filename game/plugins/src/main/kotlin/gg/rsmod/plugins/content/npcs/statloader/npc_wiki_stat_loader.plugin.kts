d15fc7afabdcf89c81883ca1b39e7c588f2507ba
Mark <kiwiszijncool@gmail.com>
2021-01-17 23:23:09 +0100

    Added combat stat loader using json with data scraped from OopenOSRS wiki-scraper

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/statloader/npc_wiki_stat_loader.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/statloader/npc_wiki_stat_loader.plugin.kts
@@ -12,55 +12,58 @@ on_world_init {
                 System.err.println("Key $key in ./data/npcwikistats.json has no value")
                 continue
             }
+            try {
 
-            set_combat_def(key) {
+                set_combat_def(key) {
 
-                //TODO NpcCombatDef.kt is missing venom and poison immunity
+                    //TODO NpcCombatDef.kt is missing venom and poison immunity
 
-                species {}
+                    species {}
 
-                configs {
-                    attackSpeed = data.attackSpeed
-                    respawnDelay = NpcCombatDef.DEFAULT.respawnDelay
+                    configs {
+                        attackSpeed = data.attackSpeed
+                        respawnDelay = NpcCombatDef.DEFAULT.respawnDelay
 
+                    }
+
+                    aggro {
+                        radius = NpcCombatDef.DEFAULT.aggressiveRadius
+                        searchDelay = NpcCombatDef.DEFAULT.aggroTargetDelay
+                    }
+
+                    stats {
+                        hitpoints = data.hitpoints
+                        attack = data.attackLevel
+                        strength = data.strengthLevel
+                        defence = data.defenceLevel
+                        magic = data.magicLevel
+                    }
+
+                    bonuses {
+                        defenceStab = data.stabDef
+                        defenceSlash = data.slashDef
+                        defenceCrush = data.crushDef
+                        defenceMagic = data.magicDef
+                        defenceRanged = data.rangeDef
+
+                        attackBonus = data.bonusAttack
+                        strengthBonus = data.bonusStrength
+                        rangedStrengthBonus = data.bonusRangeStrength
+                        magicDamageBonus = data.bonusMagicDamage
+                    }
+
+                    anims {
+                        block = NpcCombatDef.DEFAULT_BLOCK_ANIMATION
+                        death = NpcCombatDef.DEFAULT_DEATH_ANIMATION
+                    }
+
+                    slayerData {
+                        xp = NpcCombatDef.DEFAULT.slayerXp
+                        levelRequirement = data.slayerLevel
+                    }
                 }
-
-                aggro {
-                    radius = NpcCombatDef.DEFAULT.aggressiveRadius
-                    searchDelay = NpcCombatDef.DEFAULT.aggroTargetDelay
-                }
-
-                stats {
-                    hitpoints = data.hitpoints
-                    attack = data.attackLevel
-                    strength = data.strengthLevel
-                    defence = data.defenceLevel
-                    magic = data.magicLevel
-                }
-
-                bonuses {
-                    defenceStab = data.stabDef
-                    defenceSlash = data.slashDef
-                    defenceCrush = data.crushDef
-                    defenceMagic = data.magicDef
-                    defenceRanged = data.rangeDef
-
-                    attackBonus = data.bonusAttack
-                    strengthBonus = data.bonusStrength
-                    rangedStrengthBonus = data.bonusRangeStrength
-                    magicDamageBonus = data.bonusMagicDamage
-                }
-
-                anims {
-                    block = NpcCombatDef.DEFAULT_BLOCK_ANIMATION
-                    death = NpcCombatDef.DEFAULT_DEATH_ANIMATION
-
-                }
-
-                slayerData {
-                    xp = NpcCombatDef.DEFAULT.slayerXp
-                    levelRequirement = data.slayerLevel
-                }
+            } catch (e: IllegalStateException) {
+                println("Npc Wiki Stat plugin tried to define the stats of npc with id $key but they have already been set")
             }
         }
     }
